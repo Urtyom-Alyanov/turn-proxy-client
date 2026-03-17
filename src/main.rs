0003c2a0622f8cd60ac2133216fa31e92e52f42f
Urtyom-Alyanov <urtyomalyanov@gmail.com>
2026-03-18 00:11:25 +0300

    feat: add `listening.rs` boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 pub mod providers;
 pub mod logging;
 pub mod configuration;
+pub mod dtls;
+pub mod proxy_process;
 
 use crate::configuration::init_configuration::init_config;
 use crate::logging::init_logger;
---
+++ src/proxy_process/listening.rs
@@ -0,0 +1,5 @@
+use crate::configuration::configuration::AppConfiguration;
+
+pub async fn listening(config: AppConfiguration) {
+  
+}
